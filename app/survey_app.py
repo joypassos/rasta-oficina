21e0af2802eb9e0f207327a7bad6efb6f8fad1d9
Joyce Passos <joy.passos@hotmail.com>
2024-08-01 20:30:03 -0300

    commit

--- app/survey_app.py
+++ app/survey_app.py
@@ -1,34 +1,27 @@
-# survey_app.py
 from flask import Flask, request, jsonify
 import psycopg2
-import os
 
 app = Flask(__name__)
 
-# Configuração do banco de dados
-db_conn = psycopg2.connect(
-    dbname=os.getenv('DB_NAME'),
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-    host=os.getenv('DB_HOST')
-)
-db_cursor = db_conn.cursor()
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname='dbtestepq',
+        user='admin',
+        password='123456@Aadbc',
+        host='10.0.1.176'
+    )
+    return conn
 
 @app.route('/survey', methods=['POST'])
-def take_survey():
+def create_survey():
     data = request.json
-    if not data or 'response' not in data:
-        return jsonify({'error': 'Invalid data'}), 400
-    
-    db_cursor.execute("INSERT INTO survey_responses (response) VALUES (%s)", (data['response'],))
-    db_conn.commit()
-    return jsonify({'message': 'Survey response recorded!'}), 201
-
-@app.route('/results', methods=['GET'])
-def get_results():
-    db_cursor.execute("SELECT response FROM survey_responses")
-    results = db_cursor.fetchall()
-    return jsonify([r[0] for r in results])
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO surveys (question) VALUES (%s)', (data['question'],))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify({'status': 'survey created'}), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
